9fd92b90dcf0cb81506e685b1aa90736d0f87328
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 22:14:50 +0900

    조건부 로직 다형성으로 바꾸기: 메소드 이동

--- src/chap1/createStatementData.ts
+++ src/chap1/createStatementData.ts
@@ -1,8 +1,48 @@
 import { EnrichedInvoice, EnrichedPerformance, Invoice, Performance } from "@/types/Invoice";
-import { Plays } from "@/types/play";
+import { Play, Plays } from "@/types/play";
 
 class PerformanceCalculator {
-	constructor(private readonly performance: Performance) {}
+	constructor(private readonly _performance: Performance, private readonly _play: Play) {}
+
+	get performance() {
+		return this._performance;
+	}
+
+	get play() {
+		return this._play;
+	}
+
+	get amount() {
+		let result = 0;
+
+		switch (this.play.type) {
+			case "tragedy":
+				result = 40000;
+				if (this.performance.audience > 30) {
+					result += 1000 * (this.performance.audience - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (this.performance.audience > 20) {
+					result += 10000 + 500 * (this.performance.audience - 20);
+				}
+				result += 300 * this.performance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${this.play.type}`);
+		}
+		return result;
+	}
+
+	get volumeCredits() {
+		let result = 0;
+		result += Math.max(this.performance.audience - 30, 0);
+		if ("comedy" === this.play.type) {
+			result += Math.floor(this.performance.audience / 5);
+		}
+		return result;
+	}
 }
 
 export function createStatementData(invoice: Invoice, plays: Plays) {
@@ -14,47 +54,16 @@ export function createStatementData(invoice: Invoice, plays: Plays) {
 	return result;
 
 	function enrichPerformance(aPerformance: Performance) {
+		const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
 		const result = { ...aPerformance } as EnrichedPerformance;
-		result.play = playFor(result);
-		result.amount = amountFor(result);
-		result.volumeCredits = volumeCreditsFor(result);
+		result.play = calculator.play;
+		result.amount = calculator.amount;
+		result.volumeCredits = calculator.volumeCredits;
 		return result;
 	}
 
 	function playFor(aPerformance: Performance) {
 		return plays[aPerformance.playID];
-	}
-
-	function amountFor(aPerformance: EnrichedPerformance) {
-		let result = 0;
-
-		switch (aPerformance.play.type) {
-			case "tragedy":
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case "comedy":
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-		}
-		return result;
-	}
-
-	function volumeCreditsFor(aPerformance: EnrichedPerformance) {
-		let result = 0;
-		result += Math.max(aPerformance.audience - 30, 0);
-		if ("comedy" === aPerformance.play.type) {
-			result += Math.floor(aPerformance.audience / 5);
-		}
-		return result;
 	}
 
 	function totalAmount(data: EnrichedInvoice) {
