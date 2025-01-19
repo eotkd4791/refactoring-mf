10896644c21225a19f185e07d36de2c9f27142c1
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 22:02:18 +0900

    html renderer 추가

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -1,6 +1,6 @@
 import type { Invoice, EnrichedInvoice } from "@/types/Invoice";
 import type { Plays } from "@/types/play";
-import { createStatementData } from "./createStatementData";
+import { createStatementData } from "@/chap1/createStatementData";
 
 export function statement(invoice: Invoice, plays: Plays) {
 	return renderPlainText(createStatementData(invoice, plays));
@@ -16,12 +16,30 @@ function renderPlainText(data: EnrichedInvoice) {
 	result += `총액 ${usd(data.totalAmount)}\n`;
 	result += `적립 포인트: ${data.totalVolumeCredits} 점\n`;
 	return result;
+}
 
-	function usd(aNumber: number) {
-		return new Intl.NumberFormat("en-US", {
-			style: "currency",
-			currency: "USD",
-			minimumFractionDigits: 2,
-		}).format(aNumber / 100);
+function htmlStatement(invoice: Invoice, plays: Plays) {
+	return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: EnrichedInvoice) {
+	let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+	result += "<table>\n";
+	result += "<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>";
+	for (let perf of data.performances) {
+		result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience} 석)</td>`;
+		result += `<td>${usd(perf.amount)}</td></tr>\n`;
 	}
+	result += "</table>\n";
+	result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+	result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+	return result;
+}
+
+function usd(aNumber: number) {
+	return new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		minimumFractionDigits: 2,
+	}).format(aNumber / 100);
 }
