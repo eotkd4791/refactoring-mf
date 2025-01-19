05f0fa159d5c94d6bcba2e4a7938beb20e6db86a
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 17:28:43 +0900

    함수로 추출하여 임시 변수 제거

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -6,12 +6,6 @@ export function statement(invoice: Invoice, plays: Plays) {
 	let volumeCredits = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-	const format = new Intl.NumberFormat("en-US", {
-		style: "currency",
-		currency: "USD",
-		minimumFractionDigits: 2,
-	}).format;
-
 	for (let perf of invoice.performances) {
 		volumeCredits += volumeCreditsFor(perf);
 
@@ -30,6 +24,14 @@ export function statement(invoice: Invoice, plays: Plays) {
 			result += Math.floor(aPerformance.audience / 5);
 		}
 		return result;
+	}
+
+	function format(aNumber: number) {
+		return new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+			minimumFractionDigits: 2,
+		}).format(aNumber);
 	}
 
 	function amountFor(aPerformance: Performance) {
