e78df28fc02deee0193a2d3ec5ededdf44402070
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 17:33:16 +0900

    반복문 쪼개기

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -7,10 +7,12 @@ export function statement(invoice: Invoice, plays: Plays) {
 	let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
 	for (let perf of invoice.performances) {
-		volumeCredits += volumeCreditsFor(perf);
-
 		result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 		totalAmount += amountFor(perf);
+	}
+
+	for (let perf of invoice.performances) {
+		volumeCredits += volumeCreditsFor(perf);
 	}
 
 	result += `총액 ${usd(totalAmount)}\n`;
