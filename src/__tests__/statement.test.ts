ad048f5d1e0d2e8d2851e1dbc79a39d623874eac
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 22:33:57 +0900

    고정 문자열 변경

--- src/__tests__/statement.test.ts
+++ src/__tests__/statement.test.ts
@@ -20,21 +20,4 @@ describe("statement 함수", () => {
 
     expect(result).toBe(expectedResult);
   });
-
-  test("statement 스냅샷", () => {
-    const invoice = invoices[0];
-    const result = statement(invoice, plays);
-
-    expect(result).toMatchInlineSnapshot(
-      `
-			"청구 내역 (고객명: BigCo)
-			Hamlet: $650.00 (55석)
-			As You Like It: $580.00 (35석)
-			Othello: $500.00 (40석)
-			총액 $1,730.00
-			적립 포인트: 47 점
-			"
-		`,
-    );
-  });
 });
