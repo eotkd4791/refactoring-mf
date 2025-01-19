ea54a09f07d85734c879721030aae911e95fdeff
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 21:38:18 +0900

    amountFor 함수 호출 부분 - 중간 데이터를 사용하도록 수정

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -2,7 +2,7 @@ import type { Performance, Invoice } from "@/types/Invoice";
 import { Overwrite } from "@/types/libs";
 import type { Play, Plays } from "@/types/play";
 
-type EnrichedPerformance = Overwrite<Performance, { play: Play }>;
+type EnrichedPerformance = Overwrite<Performance, { play: Play; amount: number }>;
 type EnrichedInvoice = Overwrite<Invoice, { performances: EnrichedPerformance[] }>;
 
 export function statement(invoice: Invoice, plays: Plays) {
@@ -14,11 +14,35 @@ export function statement(invoice: Invoice, plays: Plays) {
 	function enrichPerformance(aPerformance: Performance) {
 		const result = { ...aPerformance } as EnrichedPerformance;
 		result.play = playFor(result);
+		result.amount = amountFor(result);
 		return result;
 	}
 
 	function playFor(aPerformance: Performance) {
 		return plays[aPerformance.playID];
+	}
+
+	function amountFor(aPerformance: EnrichedPerformance) {
+		let result = 0;
+
+		switch (aPerformance.play.type) {
+			case "tragedy":
+				result = 40000;
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
+				}
+				result += 300 * aPerformance.audience;
+				break;
+			default:
+				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+		}
+		return result;
 	}
 }
 
@@ -26,7 +50,7 @@ function renderPlainText(data: EnrichedInvoice) {
 	let result = `청구 내역 (고객명: ${data.customer})\n`;
 
 	for (let perf of data.performances) {
-		result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+		result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 	}
 
 	result += `총액 ${usd(totalAmount())}\n`;
@@ -36,7 +60,7 @@ function renderPlainText(data: EnrichedInvoice) {
 	function totalAmount() {
 		let result = 0;
 		for (let perf of data.performances) {
-			result += amountFor(perf);
+			result += perf.amount;
 		}
 		return result;
 	}
@@ -64,28 +88,5 @@ function renderPlainText(data: EnrichedInvoice) {
 			currency: "USD",
 			minimumFractionDigits: 2,
 		}).format(aNumber / 100);
-	}
-
-	function amountFor(aPerformance: EnrichedPerformance) {
-		let result = 0;
-
-		switch (aPerformance.play.type) {
-			case "tragedy":
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case "comedy":
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-		}
-		return result;
 	}
 }
