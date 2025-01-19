e9c3749a46e1acc491c3f6127f70af3fadfb075b
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 22:06:00 +0900

    불필요한 let -> const로 수정

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -9,7 +9,7 @@ export function statement(invoice: Invoice, plays: Plays) {
 function renderPlainText(data: EnrichedInvoice) {
 	let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-	for (let perf of data.performances) {
+	for (const perf of data.performances) {
 		result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 	}
 
@@ -26,7 +26,7 @@ function renderHtml(data: EnrichedInvoice) {
 	let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
 	result += "<table>\n";
 	result += "<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>";
-	for (let perf of data.performances) {
+	for (const perf of data.performances) {
 		result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience} 석)</td>`;
 		result += `<td>${usd(perf.amount)}</td></tr>\n`;
 	}
