68592757ee1e36e05ec3d71a14c70b136388f40b
eotkd4791 <eotkd4791@gmail.com>
2025-01-19 18:51:23 +0900

    totalAmout 변수 인라인화

--- src/chap1/statement.ts
+++ src/chap1/statement.ts
@@ -8,9 +8,7 @@ export function statement(invoice: Invoice, plays: Plays) {
 		result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 	}
 
-	let totalAmount = appleSauce();
-
-	result += `총액 ${usd(totalAmount)}\n`;
+	result += `총액 ${usd(appleSauce())}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()} 점\n`;
 	return result;
 
