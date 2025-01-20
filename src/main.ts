d50cbbd80235e499577cf6b35c45ab7a6917fc02
eotkd4791 <eotkd4791@gmail.com>
2025-01-20 22:12:21 +0900

    렌더 함수 수정

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { invoices } from "@/data/invoices";
 import { plays } from "@/data/plays";
-import { statement } from "@/chap1/statement";
+import { statement, htmlStatement } from "@/chap1/statement";
 import { render } from "@/utils/render";
 
 function main() {
   const invoice = invoices[0];
-  const result = statement(invoice, plays);
-  render(result);
+  const result1 = statement(invoice, plays);
+  const result2 = htmlStatement(invoice, plays);
+  render(result1 + "<br />".repeat(2) + "<hr />".repeat(2) + result2);
 }
 
 main();
