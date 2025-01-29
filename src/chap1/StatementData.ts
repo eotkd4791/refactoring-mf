128808354af8aa9144a735c4aa0ebb9a04005305
eotkd4791 <eotkd4791@gmail.com>
2025-01-29 16:39:10 +0900

    statement data 생성하는 함수 - 클래스로 변경

---
+++ src/chap1/StatementData.ts
@@ -0,0 +1,38 @@
+import type { EnrichedInvoice, EnrichedPerformance, Invoice, Performance } from "@/types/Invoice";
+import type { Plays } from "@/types/play";
+import { PerformanceCalculatorFactory } from "@/chap1/performanceCalculator/PerformanceCalculatorFactory";
+
+export class StatementData {
+  constructor(private readonly invoice: Invoice, private readonly plays: Plays) {}
+
+  create() {
+    const result = {} as EnrichedInvoice;
+    result.customer = this.invoice.customer;
+    result.performances = this.invoice.performances.map((aPerformance) => this.enrichPerformance(aPerformance));
+    result.totalAmount = this.totalAmount(result);
+    result.totalVolumeCredits = this.totalVolumeCredits(result);
+    return result;
+  }
+
+  private enrichPerformance(aPerformance: Performance): EnrichedPerformance {
+    const calculator = PerformanceCalculatorFactory.create(aPerformance, this.playFor(aPerformance));
+    return {
+      ...aPerformance,
+      play: calculator.play,
+      amount: calculator.amount,
+      volumeCredits: calculator.volumeCredits,
+    };
+  }
+
+  private playFor(aPerformance: Performance) {
+    return this.plays[aPerformance.playID];
+  }
+
+  private totalAmount({ performances }: EnrichedInvoice) {
+    return performances.reduce((total, p) => total + p.amount, 0);
+  }
+
+  private totalVolumeCredits({ performances }: EnrichedInvoice) {
+    return performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
